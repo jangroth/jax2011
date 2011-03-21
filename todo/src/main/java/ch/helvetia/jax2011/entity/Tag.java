c1e98383a43ea355010e427401a067016896d2c4
Jan Groth <asiandub@gmx.de>
2011-03-21 19:10:10 +0100

    added attributes

--- todo/src/main/java/ch/helvetia/jax2011/entity/Tag.java
+++ todo/src/main/java/ch/helvetia/jax2011/entity/Tag.java
@@ -16,4 +16,30 @@ public class Tag {
 	
 	private String name;
 
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
--- todo/src/main/java/ch/helvetia/jax2011/entity/Todo.java
+++ todo/src/main/java/ch/helvetia/jax2011/entity/Todo.java
@@ -1,5 +1,9 @@
 package ch.helvetia.jax2011.entity;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,5 +16,52 @@ public class Todo {
 	private Long id;
 	
 	private String text;
+	
+	private Date dueDate;
+	
+	private Boolean done;
+	
+	private List<Tag> tags = new ArrayList<Tag>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Boolean getDone() {
+		return done;
+	}
+
+	public void setDone(Boolean done) {
+		this.done = done;
+	}
+
+	public List<Tag> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;
+	}
+	
 
 }
