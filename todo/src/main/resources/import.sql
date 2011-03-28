e917e26ac7eb6e9f093ffa6c50b36292e8182abb
Jan Groth <asiandub@gmx.de>
2011-03-28 06:55:26 +0200

    more test data

--- todo/src/main/resources/import.sql
+++ todo/src/main/resources/import.sql
@@ -1,2 +1,12 @@
-insert into TODO ("ID", "NAME", "DESCRIPTION", "DUEDATE") values(10000, 'prepare application', 'many more usecases are needed', DATE '2012-12-31');
-insert into TODO ("ID", "NAME", "DESCRIPTION", "DUEDATE") values(10001, 'document code', 'this is alway important', DATE '2011-12-31'); 
+insert into TODO ("ID", "NAME", "DESCRIPTION", "DUEDATE", "DONE") values (10000, 'prepare application', 'many more usecases are needed', DATE '2012-12-31', false);
+insert into TODO ("ID", "NAME", "DESCRIPTION", "DUEDATE", "DONE") values (10001, 'document code', 'this is alway important', DATE '2011-12-31', false);
+insert into TODO ("ID", "NAME", "DESCRIPTION", "DUEDATE", "DONE") values (10002, 'call mum', 'dont forget her birthday this year', DATE '2012-08-25', false);
+
+insert into TAG ("ID", "NAME", "DESCRIPTION") values (20000, '@work', 'everything at work');
+insert into TAG ("ID", "NAME", "DESCRIPTION") values (20001, '@phone', 'all phone calls to make');
+insert into TAG ("ID", "NAME", "DESCRIPTION") values (20002, '@code', 'everything related to source code');
+
+insert into TODO_TAG ("TODO_ID", "TAGS_ID") values (10000, 20000);
+insert into TODO_TAG ("TODO_ID", "TAGS_ID") values (10000, 20002);
+insert into TODO_TAG ("TODO_ID", "TAGS_ID") values (10001, 20002);
+insert into TODO_TAG ("TODO_ID", "TAGS_ID") values (10002, 20001);
