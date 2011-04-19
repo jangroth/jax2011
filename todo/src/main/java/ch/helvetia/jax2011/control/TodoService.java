1557f0d7b0b826e3a1058332d353eb581cffa7a7
Jan Groth <asiandub@gmx.de>
2011-04-19 14:25:58 +0200

    added beanvalidation

--- todo/src/main/java/ch/helvetia/jax2011/control/TodoService.java
+++ todo/src/main/java/ch/helvetia/jax2011/control/TodoService.java
@@ -2,16 +2,22 @@ package ch.helvetia.jax2011.control;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.jboss.logging.Logger;
 
 import ch.helvetia.jax2011.common.stereotypes.Service;
 import ch.helvetia.jax2011.db.TodoDb;
 import ch.helvetia.jax2011.entity.Todo;
+import ch.helvetia.jax2011.error.ValidationException;
 
 /**
  * Service for Todos.
@@ -35,10 +41,24 @@ public class TodoService {
 	}
 
 	public void saveTodo(Todo todo) {
+		// Use Seam REST for validation
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<Todo>> violations = validator.validate(todo);
+		if (violations.size() > 0) {
+			throw new ValidationException("Validation failed");
+		}
 		em.persist(todo);
 	}
 
 	public void updateTodo(Todo todo) {
+		// Use Seam REST for validation
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<Todo>> violations = validator.validate(todo);
+		if (violations.size() > 0) {
+			throw new ValidationException("Validation failed");
+		}
 		em.merge(todo);
 	}
 
