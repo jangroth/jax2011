a7da9b59920fe0a0003caeb5ceba1d3d366dc3a3
Andreas Eigenmann <andreas@eigenmann.co>
2011-04-28 10:55:48 +0200

    use BeanManagerLocator from solder, implemented getInstance

--- todo/src/main/java/ch/helvetia/jax2011/util/BeanManagerUtil.java
+++ todo/src/main/java/ch/helvetia/jax2011/util/BeanManagerUtil.java
@@ -1,8 +1,11 @@
 package ch.helvetia.jax2011.util;
 
+import java.util.Set;
+
+import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+
+import org.jboss.seam.solder.beanManager.BeanManagerLocator;
 
 /**
  * A utility for use in non-managed classes, which are not able to obtain a
@@ -11,18 +14,27 @@ import javax.naming.NamingException;
  * TODO Use Seam Solder
  */
 public class BeanManagerUtil {
-	
-	public static BeanManager getBeanManager() {
-        try {
-            return (BeanManager) new InitialContext().lookup("java:comp/BeanManager");
-        } catch (NamingException e) {
-            try {
-                return (BeanManager) new InitialContext().lookup("java:app/BeanManager");
-            } catch (NamingException e1) {
-                throw new RuntimeException(e1);
-            }
 
-        }
-    }
+	public static BeanManager getBeanManager() {
+		return new BeanManagerLocator().getBeanManager();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T getInstance(Class<T> beanClass) {
+		BeanManager manager = getBeanManager();
+		Set<Bean<?>> beans = manager.getBeans(beanClass);
+		if (beans.size() != 1) {
+			if (beans.size() == 0) {
+				throw new RuntimeException("No beans of class "
+						+ beanClass + " found.");
+			} else {
+				throw new RuntimeException("Multiple beans of class "
+						+ beanClass + " found: " + beans + ".");
+			}
+		}
+		Bean<T> myBean = (Bean<T>) beans.iterator().next();
+		return (T) manager.getReference(myBean, beanClass,
+				manager.createCreationalContext(myBean));
+	}
 
 }
