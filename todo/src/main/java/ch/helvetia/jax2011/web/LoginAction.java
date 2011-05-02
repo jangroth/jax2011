0c434e772abd0db2aa9c4704639c634f500061cd
Jan Groth <asiandub@gmx.de>
2011-05-02 11:57:18 +0200

    commit fuer messageprogagation aus i18n

--- todo/src/main/java/ch/helvetia/jax2011/web/LoginAction.java
+++ todo/src/main/java/ch/helvetia/jax2011/web/LoginAction.java
@@ -2,20 +2,15 @@ package ch.helvetia.jax2011.web;
 
 import java.io.Serializable;
 
-import javax.enterprise.event.Observes;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
+import org.jboss.seam.international.status.Messages;
+import org.jboss.seam.international.status.builder.BundleKey;
 import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.Identity;
-import org.jboss.seam.security.events.LoggedInEvent;
-import org.jboss.seam.security.events.LoginFailedEvent;
-import org.jboss.seam.servlet.event.Initialized;
-import org.jboss.seam.servlet.http.HttpServletRequestContext;
 
 import ch.helvetia.jax2011.common.stereotypes.Action;
-import ch.helvetia.jax2011.util.MessageHelper;
 
 /**
  * Action to login
@@ -29,20 +24,23 @@ public class LoginAction implements Serializable {
 	@Inject
 	private Identity identity;
 
-	// TODO Use Seam 3
+	@Inject
+	private Messages messages;
+
 	public String login() {
 		identity.login();
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		if (identity.isLoggedIn()) {
-			facesContext.addMessage(null, MessageHelper.createMessageFromKey(
-					FacesMessage.SEVERITY_INFO, "loginWelcome",
-					credentials.getUsername()));
+			messages.info(new BundleKey("messages", "loginWelcome"),
+					credentials.getUsername());
+			// facesContext.addMessage(null, MessageHelper.createMessageFromKey(
+			// FacesMessage.SEVERITY_INFO, "loginWelcome",
+			// credentials.getUsername()));
 			facesContext.getExternalContext().getFlash().setKeepMessages(true);
 			return "home.xhtml?faces-redirect=true";
 		} else {
-			facesContext.addMessage(null, MessageHelper.createMessageFromKey(
-					FacesMessage.SEVERITY_ERROR, "loginFailed",
-					credentials.getUsername()));
+			messages.warn(new BundleKey("messages", "loginFailed"),
+					credentials.getUsername());
 			return null;
 		}
 	}
