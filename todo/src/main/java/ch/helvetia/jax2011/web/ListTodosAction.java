f7f0c9db59ea9caeac0eb45c11f3e64d4cf60c92
Jan Groth <asiandub@gmx.de>
2011-05-05 22:34:01 +0200

    removed magic exception

--- todo/src/main/java/ch/helvetia/jax2011/web/ListTodosAction.java
+++ todo/src/main/java/ch/helvetia/jax2011/web/ListTodosAction.java
@@ -17,7 +17,6 @@ import ch.helvetia.jax2011.boundary.ListTodosTask;
 import ch.helvetia.jax2011.common.stereotypes.Action;
 import ch.helvetia.jax2011.entity.Tag;
 import ch.helvetia.jax2011.entity.Todo;
-import ch.helvetia.jax2011.error.TodoException;
 
 /**
  * Action to create a new todo-item.
@@ -43,7 +42,6 @@ public class ListTodosAction implements Serializable {
 				&& conversation.isTransient()) {
 			task.init();
 		}
-		throw new TodoException();
 	}
 
 	public void refresh() {
